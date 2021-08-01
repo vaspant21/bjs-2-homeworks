98b87d592448e1fc08ce1bc78c39fd3a05b45ba0
vaspant1 <panteleev.vasily@gmail.com>
2021-08-01 11:09:37 -0400

    Initial commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,26 @@
+'use strict'
 function solveEquation(a, b, c) {
   let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
-}
+  const discriminant = Math.pow(b, 2) - 4 * a * c; 
+    if (discriminant < 0) {
+      console.log(`Количество действительных корней равно 0.`);
+    } else if (discriminant === 0) {
+      const singleRoot = -b / (2 * a);
+      arr = [singleRoot];
+    } else if (discriminant > 0) {
+      const rootOne = (-b - Math.sqrt(discriminant)) / (2 * a);
+      const rootTwo = (-b + Math.sqrt(discriminant)) / (2 * a);
+      arr = [rootOne, rootTwo];
+    };
+  return arr
+}; 
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//   let totalAmount;
 
-  return totalAmount;
-}
+//   // код для задачи №2 писать здесь
+
+//   return totalAmount;
+// }
+
