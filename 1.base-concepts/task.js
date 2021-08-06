02a23fe54b83bc41d7b951a27447fcabf257a58b
vaspant1 <panteleev.vasily@gmail.com>
2021-08-06 15:27:46 -0400

    Correction 3

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -23,8 +23,36 @@ function solveEquation(a, b, c) {
 
 function calculateTotalMortgage (creditRate, downPayment, creditAmount, payOffDate) {
   let creditBody = creditAmount - downPayment;
+  let monthlyRate = ((creditRate / 100) / 12);
+
+  if (creditRate >= 0 && typeof creditRate === "number") {
+    creditRate = true;
+  } else if {
+    console.log(`Параметр "Процентная ставка" содержит неправильное значение "test".`);
+  };
+
+  if (downPayment >= 0 && typeof downPayment === "number") {
+    downPayment = true;
+  } else {
+    console.log(`Параметр "Начальный взнос" содержит неправильное значение "test".`);
+  };
+
+  if (creditAmount >= 0 && typeof creditAmount === "number") {
+    creditAmount = true;
+  } else {
+    console.log(`Параметр "Общая стоимость" содержит неправильное значение "test".`);
+  };
+
+  if (payOffDate >= new Date ()) {
+      payOffDate = true;
+  } else {
+    console.log(`Ошибка! Дата погашения кредита не может быть ранее текущей даты "test".`);
+  };
+
   let numOfMonths = (new Date (payOffDate) - new Date ()) / 1000 / 60 / 60 / 24 / 30.5;
-  let totalDebt = 0;
-  let monthlyPayment = creditAmount * ((creditRate / 100 / 12) + ((creditRate / 100 / 12)/(Math.pow((1 + (creditRate / 100 / 12)), 12) - 1)));
-  return totalDebt = (monthlyPayment * numOfMonths);
+  let monthlyPayment = creditBody * (monthlyRate + (monthlyRate /(Math.pow((1 + monthlyRate), numOfMonths) - 1)));
+  let totalDebt = (monthlyPayment * numOfMonths);
+  console.log(numOfMonths);
+  return Number(totalDebt.toFixed(0));
+
 };
