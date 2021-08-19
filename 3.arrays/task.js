d0ecb5a7fb859dbafbded5360c79e8096603f64d
vaspant21 <panteleev.vasily@gmail.com>
2021-08-19 09:39:41 -0400

    Correction 1

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -3,10 +3,11 @@
 //--------------TASK 1--------------
 
 function compareArrays(arr1, arr2) {
-  if (arr1.length != arr2.length) {
+  /* if (arr1.length != arr2.length) {
     return false;
   }
-  return arr1.every((element, index) => element === arr2[index])
+  return arr1.every((element, index) => element === arr2[index]); */
+  return arr1.length === arr2.length && arr1.every((element, index) => element === arr2[index]);
 }
 
 console.log(compareArrays([8, 9], [6])) // false, разные значения
